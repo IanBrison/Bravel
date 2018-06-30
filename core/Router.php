eb1ab51a7a66992182a8170771cd4556bb69ce87
IanBrison <ianbrison222@gmail.com>
2018-07-01 00:26:26 +0900

    create the Router class

---
+++ core/Router.php
@@ -0,0 +1,46 @@
+<?php
+
+class Router {
+
+    protected $routes;
+
+    public function __construct($definitions) {
+        $this->routes = $this->compileRoutes($definitions);
+    }
+
+    public function compileRoutes($definitions) {
+        $routes = array();
+
+        foreach ($definitions as $url => $params) {
+            $tokens = explode('/', ltrim($url, '/'));
+            foreach ($tokens as $i => $token) {
+                if (0 === strpos($token, ':') {
+                    $name = substr($token, 1);
+                    $token = '(?P<' . $name . '>[^/]+)';
+                }
+                $tokens[$i] = $token;
+            }
+
+            $pattern = '/' . implode('/', $tokens);
+            $routes[$pattern] = $params;
+        }
+
+        return $routes;
+    }
+
+    public function resolve($path_info) {
+        if ('/' !== substr($path_info, 0, 1)) {
+            $path_info = '/' . $path_info;
+        }
+
+        foreach ($this->routes as $pattern => $params) {
+            if (preg_match('#^' . $pattern . '$#', $path_info, $matches)) {
+                $params = array_merge($params, $matches);
+
+                return $params;
+            }
+        }
+
+        return false;
+    }
+}
