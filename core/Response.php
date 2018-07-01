47aaffc57bf34fce09a7f82effd9a6b3f9925d77
IanBrison <ianbrison222@gmail.com>
2018-07-01 14:49:42 +0900

    create the Response class

---
+++ core/Response.php
@@ -0,0 +1,32 @@
+<?php
+
+class Response {
+
+    protected $content;
+    protected $status_code = 200;
+    protected $status_text = 'OK';
+    protected $http_headers = array();
+
+    public function send() {
+        header('HTTP/1.1 ' . $this->status_code . ' ' . $this->status_text);
+
+        foreach ($this->http_headers as $name => $value) {
+            header($name . ': ' . $value);
+        }
+
+        echo $this->content;
+    }
+
+    public function setContent($content) {
+        $this->content = $content;
+    }
+
+    public function setStatusCode($status_code, $status_text = '') {
+        $this->status_code = $status_code;
+        $this->status_text = $status_text;
+    }
+
+    public function setHttpHeader($name, $value) {
+        $this->http_headers[$name] = $value;
+    }
+}
