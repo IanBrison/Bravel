94888e823424bddd482d751d09977e86ddc26d49
IanBrison <ianbrison222@gmail.com>
2018-07-01 19:28:38 +0900

    add the 404 error exception

--- core/Application.php
+++ core/Application.php
@@ -77,15 +77,19 @@ abstract class Application {
     }
 
     public function run() {
-        $params = $this->router->resolve($this->request->getPathInfo());
-        if ($params === false) {
+        try {
+            $params = $this->router->resolve($this->request->getPathInfo());
+            if ($params === false) {
+                throw new HttpNotFoundException('No route found for ' . $this->request->getPathInfo());
+            }
 
+            $controller = $params['controller'];
+            $action = $params['action'];
+
+            $this->runAction($controller, $action, $params);
+        } catch (HttpNotFoundException $e) {
+            $this->render404Page($e);
         }
-
-        $controller = $params['controller'];
-        $action = $params['action'];
-
-        $this->runAction($controller, $action, $params);
 
         $this->response->send();
     }
@@ -95,7 +99,7 @@ abstract class Application {
 
         $controller = $this->findController($controller_class);
         if ($controller === false) {
-
+            throw new HttpNotFoundException($controller_class . ' controller is not found.');
         }
 
         $content = $controller->run($action, $params);
@@ -118,5 +122,27 @@ abstract class Application {
         }
 
         return new $controller_class($this);
+    }
+
+    protected function render404Page($e) {
+        $this->response->setStatusCode(404, 'Not Found');
+        $message = $this->isDebugMode() ? $e->getMessage() : 'Page not found.';
+        $message = htmlspecialchars($message, ENT_QUOTES, 'UTF-8');
+
+        $this->response->setContent(<<<EOF
+<!DOCTYPE html PUBLIC "~//W3C//DTD XHTML 1.0 Transitional//EN"
+"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <title>404</title>
+</head>
+<body>
+    {$message}
+</body>
+</html>
+EOF
+        );
+    }
     }
 }
