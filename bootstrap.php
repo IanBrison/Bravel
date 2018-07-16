2ac4b52bea63c56ea5203d52331ce987bff4e47d
IanBrison <ianbrison222@gmail.com>
2018-07-16 13:42:13 +0900

    introduce composer and remove the classloader and use composers one

--- bootstrap.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-require 'core/ClassLoader.php';
-
-$loader = new ClassLoader();
-$loader->registerDir(dirname(__FILE__).'/core');
-$loader->registerDir(dirname(__FILE__).'/models');
-$loader->register();
--- web/index.php
+++ web/index.php
@@ -1,6 +1,9 @@
 <?php
 
-require '../bootstrap.php';
+// load the autoloader that is composer
+require '../vendor/autoload.php';
+
+// laod the main Class for the application
 require '../Application.php';
 
 $app = new Application(true);
