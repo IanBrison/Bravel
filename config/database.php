8c4bf7a902ad6e5877578b5191707482c82d600d
IanBrison <ianbrison222@gmail.com>
2018-07-23 23:57:14 +0900

    add comment to database config file

--- config/database.php
+++ config/database.php
@@ -1,4 +1,9 @@
 <?php
+
+/* 
+ * config file for setting the database connection
+ * all configurations go in to a PDO instance
+ */
 return [
     'master' => [
         'dsn'      => 'mysql:dbname=braveldb;host=mariadb',
