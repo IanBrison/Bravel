1ac6626f468997c73c78d659eda22cca10d14b0c
IanBrison <ianbrison222@gmail.com>
2018-12-18 00:41:57 +0900

    stop using default values for the view

--- core/View/View.php
+++ core/View/View.php
@@ -4,8 +4,6 @@ namespace Core\View;
 
 use Core\Environment\Environment;
 use Core\Di\DiContainer as Di;
-use Core\Session\Session;
-use Core\Request\Request;
 
 class View {
 
@@ -24,7 +22,7 @@ class View {
     public function render($_path, $_variables = array(), $_layout = false) {
         $_file = $this->base_dir . '/' . $_path . '.php';
 
-        extract(array_merge($this->getDefaultValues(), $_variables));
+        extract($_variables);
 
         ob_start();
         ob_implicit_flush(0);
@@ -46,15 +44,5 @@ class View {
 
     public function escape($string) {
         return htmlspecialchars($string, ENT_QUOTES, 'UTF-8');
-    }
-
-    private function getDefaultValues(): array {
-        $request = Di::get(Request::class);
-        $session = Di::get(Session::class);
-        return array(
-            'request'  => $request,
-            'base_url' => $request->getBaseUrl(),
-            'session'  => $session,
-        );
     }
 }
