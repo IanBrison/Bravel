ebb8ed3c122e7751656f61facae4d799bee97c6f
IanBrison <ianbrison222@gmail.com>
2018-12-29 18:06:34 +0900

    change the example controller to adapt to twig

--- app/controllers/ExampleController.php
+++ app/controllers/ExampleController.php
@@ -7,6 +7,8 @@ use Core\Controller\Controller;
 class ExampleController extends Controller {
 
     public function getWelcome() {
-        return $this->render('welcome', [], null);
+        $variables = [
+        ];
+        return $this->render('welcome.twig', $variables);
     }
 }
