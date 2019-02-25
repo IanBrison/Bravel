7a12c76d6da063c4fdf462b83d3d27449540138e
IanBrison <ianbrison222@gmail.com>
2019-02-25 23:42:23 +0900

    clean a test and add a test

---
+++ test/Unit/Services/ExpampleServiceTest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Test\Unit\Services;
+
+use \Mockery;
+use Test\TestCase;
+use Core\Di\DiContainer as Di;
+use App\Models\ExampleModel;
+use App\Repositories\ExampleRepository;
+use App\Services\ExampleService;
+use Presentation\Models\WelcomeInfo;
+
+/**
+ * @coversDefaultClass \App\Services\ExampleService
+ */
+class ExampleServiceTest extends TestCase {
+
+    /**
+     * @test
+     * @covers ::getWelcomeInfo
+     */
+    public function testgetWelcomeInfoReturnsViewModel() {
+        $exampleModel = Mockery::mock(ExampleModel::class);
+        $exampleRepository = Mockery::mock(ExampleRepository::class);
+        $exampleRepository->shouldReceive('getExampleModel')->andReturn($exampleModel);
+        $welcomeInfoViewModel = Mockery::mock(WelcomeInfo::class);
+        $welcomeInfoViewModel->shouldReceive('connectionStatus')->andReturn('test');
+
+        Di::set(ExampleRepository::class, $exampleRepository);
+        Di::set(WelcomeInfo::class, $welcomeInfoViewModel);
+
+        $exampleService = new ExampleService();
+
+        $this->assertSame('test', $exampleService->getWelcomeInfo()->connectionStatus());
+    }
+}
--- test/Unit/ViewModels/WelcomeInfoTest.php
+++ test/Unit/ViewModels/WelcomeInfoTest.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Test\Unit;
+namespace Test\Unit\ViewModels;
 
 use \Mockery;
 use Test\TestCase;
