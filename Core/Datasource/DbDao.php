d8559e229e50bf392f21232ea88870d92561242f
IanBrison <ianbrison222@gmail.com>
2019-03-28 23:38:12 +0900

    add string type hint for sql

--- Core/Datasource/DbDao.php
+++ Core/Datasource/DbDao.php
@@ -24,18 +24,18 @@ abstract class DbDao {
         return (int)$id;
     }
 
-    public function execute($sql, $params = array()) {
+    public function execute(string $sql, $params = array()) {
         $stmt = Di::get(DbManager::class)->getConnection($this->connectionName)->prepare($sql);
         $stmt->execute($params);
 
         return $stmt;
     }
 
-    public function fetch($sql, $params = array()) {
+    public function fetch(string $sql, $params = array()) {
         return $this->execute($sql, $params)->fetch(PDO::FETCH_ASSOC);
     }
 
-    public function fetchAll($sql, $params = array()): array {
+    public function fetchAll(string $sql, $params = array()): array {
         return $this->execute($sql, $params)->fetchAll(PDO::FETCH_ASSOC);
     }
 }
