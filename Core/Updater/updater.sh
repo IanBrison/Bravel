9f8f83c97a8e0d7b946b409cab45c7396c729cc7
IanBrison <ianbrison222@gmail.com>
2019-04-02 23:37:45 +0900

    delete the copy bravel after update

--- Core/Updater/updater.sh
+++ Core/Updater/updater.sh
@@ -1,9 +1,8 @@
-if [ ! -d "LatestBravel" ]; then
-    git clone https://github.com/IanBrison/Bravel.git LatestBravel
-else
-    cd LatestBravel && git pull origin master && cd ..
-fi
+#!/bin/bash
+git clone https://github.com/IanBrison/Bravel.git LatestBravel
 
 cp -r ./LatestBravel/server ../../
 cp ./LatestBravel/web/index.php ../../web/
 cp -r ./LatestBravel/core ../../
+
+rm -rf ./LatestBravel
