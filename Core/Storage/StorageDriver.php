dd97dab08f97c248256f02bd09fcfecf25e72dd8
IanBrison <ianbrison222@gmail.com>
2019-04-03 00:07:22 +0900

    add a list function to the storage driver

--- Core/Storage/StorageDriver.php
+++ Core/Storage/StorageDriver.php
@@ -4,6 +4,8 @@ namespace Core\Storage;
 
 interface StorageDriver {
 
+    public function list(): array;
+
     public function save(File $file, string $fileName): bool;
 
     public function url(string $fileName): string;
--- Core/Storage/StorageDriver/LocalDriver.php
+++ Core/Storage/StorageDriver/LocalDriver.php
@@ -10,13 +10,30 @@ class LocalDriver implements StorageDriver {
 
     const LOCAL_STORAGE_DIRECTORY = '/web';
 
-    private $urlDir;
-    private $baseDir;
+    private $urlDir; // the public directory path to access the storage directory
+    private $baseDir; // the private directory path to access the storage directory
 
     public function __construct(string $path) {
         $this->urlDir = Environment::getConfig('storage.drivers.Local.basePath') . $path . '/';
         $this->baseDir = Environment::getDir(self::LOCAL_STORAGE_DIRECTORY . $this->urlDir);
         if (!is_dir($this->baseDir)) mkdir($this->baseDir, 0777, true);
+    }
+
+    public function list(): array {
+        return $this->scanDirectory('');
+    }
+
+    private function scanDirectory(string $directoryPath): array {
+        $scannedList = array_diff(scandir($this->baseDir . $directoryPath), array('..', '.'));
+        $fileList = [];
+        foreach ($scannedList as $scannedListItem) {
+            if (is_dir($scannedListItem)) {
+                array_merge($fileList, $this->scanDirectory($this->baseDir . $directoryPath . $scannedListItem));
+            } else {
+                $fileList[] = $directoryPath . $scannedListItem;
+            }
+        }
+        return $fileList;
     }
 
     public function save(File $file, string $fileName): bool {
