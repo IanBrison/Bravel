4a79611f38c554513d8ff5e5844eac8ed27d16be
IanBrison <ianbrison222@gmail.com>
2019-04-03 21:20:39 +0900

    fix the misspell

--- Core/Presenter/View.php
+++ Core/Presenter/View.php
@@ -11,14 +11,14 @@ use Core\Presenter\BuiltIns\ViewModels\CsrfToken;
 
 class View {
 
-    const BRAVEL_CORE_TEMLATE_DIRECTORY = '/core/Presenter/BuiltIns/Views';
+    const BRAVEL_CORE_TEMPLATE_DIRECTORY = '/core/Presenter/BuiltIns/Views';
 
     protected $twig; // the twig instance itself
     protected $extension; // the extension of the template files
 
     public function __construct() {
         $baseDir = Environment::getDir(Environment::getConfig('view.base_path'));
-        $bravelDir = Environment::getDir(self::BRAVEL_CORE_TEMLATE_DIRECTORY);
+        $bravelDir = Environment::getDir(self::BRAVEL_CORE_TEMPLATE_DIRECTORY);
         $templateDirectories = array(
             $baseDir,
             $bravelDir,
