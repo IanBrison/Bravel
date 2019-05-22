2eaefe3773c3d2e650014eaba37e6e8e5303b97a
IanBrison <ianbrison222@gmail.com>
2019-05-22 23:51:54 +0900

    add phpdoc to storage component

--- Core/Storage/Storage.php
+++ Core/Storage/Storage.php
@@ -4,15 +4,25 @@ namespace Core\Storage;
 
 use Core\Environment\Environment;
 use Core\Storage\StorageDriver\LocalDriver;
+use Exception;
 
 class Storage {
 
     private $locations;
 
+    /**
+     * Storage constructor.
+     * @throws Exception
+     */
     public function __construct() {
         $this->locations = Environment::getConfig('storage.locations');
     }
 
+    /**
+     * @param string $location
+     * @return StorageDriver
+     * @throws Exception
+     */
     public function location(string $location): StorageDriver {
         $driverName = null;
         if (!isset($this->locations[$location]['driver'])) {
